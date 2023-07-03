5b25ca32d0f508cdd445686dcc866f200c9ad5ec
Carl Jerson D. Oquila <Cj@dcg.ph>
2023-07-03 15:54:35 +0800

    back to old

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ app.use(bodyParser.json())
 
 
 
-// const readRouter = require('./routes/read')
-// app.use('/read', readRouter)
+const readRouter = require('./routes/read')
+app.use('/read', readRouter)
 
-// const readOneRouter = require('./routes/readOne')
-// app.use('/read', readOneRouter)
+const readOneRouter = require('./routes/readOne')
+app.use('/read', readOneRouter)
 
-// const deleteRouter = require('./routes/delete')
-// app.use('/delete', deleteRouter)
+const deleteRouter = require('./routes/delete')
+app.use('/delete', deleteRouter)
 
-// const createRouter = require('./routes/create')
-// app.use('/create', createRouter)
+const createRouter = require('./routes/create')
+app.use('/create', createRouter)
 
-// const updateRouter = require('./routes/update')
-// app.use('/update', updateRouter)
+const updateRouter = require('./routes/update')
+app.use('/update', updateRouter)
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>console.log(`Listening to port ${port}`))
